0594c62c8ffadcf3a831a67b25aefffe9fc29f77
prabhjeevnijjar <prabhjeevnijjar@gmail.com>
2020-11-25 11:02:35 +0530

    completed assignment

--- Coursera_backend_assignment_1/dishRouter.js
+++ Coursera_backend_assignment_1/dishRouter.js
@@ -1,14 +1,30 @@
 const router = require("express").Router();
-router.get(":dishId",(req,res)=>{
+router.get("/",(req,res)=>{
+    res.send('Will send all the dishes to you now!');
+});
+router.put("/",(req,res)=>{
+    res.send('PUT operation not supported on /dishes');
+});
+router.post("/",(req,res)=>{
+    res.send('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+router.delete("/",(req,res)=>{
+    res.send('Deleting all dishes');
+});
 
-})
-router.put(":dishId",(req,res)=>{
-
-})
-router.post(":dishId",(req,res)=>{
-
-})
-router.delete(":dishId",(req,res)=>{
-
-})
+router.get("/:dishId",(req,res)=>{
+    res.send('Will send leader with leader id: !'+req.params.dishId);
+});
+router.put("/:dishId",(req,res)=>{
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+        res.end('Will update the dish: ' + req.body.name +
+            ' with details: ' + req.body.description);
+});
+router.post("/:dishId",(req,res)=>{
+    res.send('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+router.delete("/:dishId",(req,res)=>{
+    res.send('Deleting all dishes with dish id :'+req.params.dishId);
+});
 module.exports = router;
+//dishId
